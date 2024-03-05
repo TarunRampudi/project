2a97edb3bb1befd84b44f5ce7509f9f0d89d9fdf
rampudi.tarun <rampuditarun@gmail.com>
2024-03-05 09:45:27 +0530

    complete

--- client/src/components/Screens/Homescreen.js
+++ client/src/components/Screens/Homescreen.js
@@ -1,24 +1,10 @@
 import React from 'react'
-<<<<<<< HEAD:client/src/components/Screens/Homescreen.js
-import menuitems from '../../fooditemsdata'
-import Items from '../Pizza'
-=======
 import pizzas from '../../fooditemsdata'
-import Pizza from '../Items'
->>>>>>> 3ec255c6fcc69f24b0c21a5bd6ac0190a4a8af43:client/src/components/Screens/Menu.js
+import Pizza from '../Pizza'
 export default function itemsscreen() {
   return (
     <div>
       <div className="row">
-<<<<<<< HEAD:client/src/components/Screens/Homescreen.js
-        {menuitems.map((menuitem, index) => {
-          return <div className='col-md-4' key={index}>
-            <div>
-              <Items menuitem={menuitem}/>
-            </div>
-          </div>
-        })}
-=======
         {pizzas.map(pizza=>{
           return (<div className='col-md-4'>
             <div>
@@ -26,7 +12,6 @@ export default function itemsscreen() {
               </div>
             </div>
         )})}
->>>>>>> 3ec255c6fcc69f24b0c21a5bd6ac0190a4a8af43:client/src/components/Screens/Menu.js
       </div>
     </div>
   )
--- client/src/fooditemsdata.js
+++ client/src/fooditemsdata.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-const  Pizzas=[
-=======
 const pizza =[
->>>>>>> 3ec255c6fcc69f24b0c21a5bd6ac0190a4a8af43
     {
 name:"PEPPER BARBECUE CHICKEN",
 varients:["small","medium" ,"large"],
@@ -17,42 +13,8 @@ category : "nonveg",
 image: "https://i.insider.com/59c9763224884966f6225939?width=1200&format=jpeg",
 description :"Pepper Barbecue Chicken I Cheese",
 },
-<<<<<<< HEAD
-// {
-//     "name":"PEPPER BARBECUE",
-//     "varients":["small","medium" ,"large"],
-//     "prices" :[
-//         {
-//     "small":200,
-//     "medium" :350,
-//     "large" :400
-//         }
-//     ],
-//     "category" : "nonveg",
-//     "image": "https://i.insider.com/59c9763224884966f6225939?width=1200&format=jpeg",
-//     "description" :"Pepper Barbecue Chicken I Cheese",
-//     },
-//     {
-//         "name":"PEPPER BARBECUE CHICKEN",
-//         "varients":["small","medium" ,"large"],
-//         "prices" :[
-//             {
-//         "small":200,
-//         "medium" :350,
-//         "large" :400
-//             }
-//         ],
-//         "category" : "nonveg",
-//         "image": "https://th.bing.com/th/id/OIP.EJEpu_XxVdtZItR1esVHpQHaE7?rs=1&pid=ImgDetMain",
-//         "description" :"Pepper Barbecue Chicken I Cheese",
-//         }
-    ];
-
-export default Pizzas;
-=======
-
 {
-    "name":"PEPPER BARBECUE CHICKEN",
+    "name":"PEPPER BARBECUE",
     "varients":["small","medium" ,"large"],
     "prices" :[
         {
@@ -82,4 +44,3 @@ export default Pizzas;
     ];
 
 export default pizza;
->>>>>>> 3ec255c6fcc69f24b0c21a5bd6ac0190a4a8af43
